b5f5debc92e98b58d88a930fde7532c5b049f6bd
Michael Hank <Michael Hank>
2017-01-21 15:06:34 -0500

    Added folders and water rises and kills player (resets level) when it rises too high

---
+++ Baby_Consumer_2000/Assets/Scripts/WaterRise.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterRise : MonoBehaviour {
+    private Vector3 startingPoint;
+    private Vector3 endingPoint;
+    public float speed = .7F;
+    private float startTime;
+    private float journeyLength;
+    private GameObject player;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        startingPoint = transform.position;
+        endingPoint = new Vector3(transform.position.x, 25, transform.position.z);
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startingPoint, endingPoint);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float timeCount = 0f;
+        transform.position = Vector3.Lerp(startingPoint, new Vector3(transform.position.x, 25, transform.position.z), timeCount);
+        timeCount = timeCount + .01f;
+        if (timeCount > 1)
+            timeCount = 1;
+
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startingPoint, endingPoint, fracJourney);
+
+        if (player.transform.position.y + .5f <= transform.position.y)
+        {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+}
